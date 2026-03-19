10cd1c1163a42cc51d906ce6715ac7225d8432c1
DESKTOP-86P23S3\Joakor <purgal-adam@wp.pl>
2026-03-19 01:01:20 +0100

    LAB 2 part 2

---
+++ Python/mysite/polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f"To jest pytanie nr {question_id}")
